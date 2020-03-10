accbe5a791d23fd1730619cfb54e94cd4d0cd9a9
Andy Edwards <jedwards@fastmail.com>
2020-03-10 16:21:00 -0500

    refactor: remove cssToJss, use convertCssToJssString in demo

--- src/css-to-jss.ts
+++ src/css-to-jss.ts
@@ -1,2 +1,21 @@
-import cssToJss from './cssToJss'
-module.exports = cssToJss
+import { FileInfo, API, Options } from 'jscodeshift'
+import { convertCssToJssString } from './convertCssToJss'
+
+module.exports = function cssToJss(
+  fileInfo: FileInfo,
+  api: API,
+  options: Options
+): string | null | undefined | void {
+  const selectionStart = options.selectionStart
+    ? parseInt(options.selectionStart)
+    : 0
+  const selectionEnd = options.selectionEnd
+    ? parseInt(options.selectionEnd)
+    : fileInfo.source.length
+
+  const before = fileInfo.source.substring(0, selectionStart)
+  const rawCss = fileInfo.source.substring(selectionStart, selectionEnd)
+  const after = fileInfo.source.substring(selectionEnd)
+
+  return `${before}${convertCssToJssString(rawCss)}${after}`
+}
